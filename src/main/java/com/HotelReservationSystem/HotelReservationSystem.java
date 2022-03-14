5a76a4bf534f57050fb90d0c158e16ae59955f95
Avinash <aj912873@gmail.com>
2022-03-14 08:44:18 +0530

    Added: Welcome TO Hotel Reservation Program

---
+++ src/main/java/com/HotelReservationSystem/HotelReservationSystem.java
@@ -0,0 +1,8 @@
+package com.HotelReservationSystem;
+
+public class HotelReservationSystem {
+	public static void main(String[] Args) {
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+
+}
